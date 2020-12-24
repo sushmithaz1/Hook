83495b82d8ae364b0001f042ba364044888c8405
sushmitha <sushmithakori99@gmail.com>
2020-12-24 18:20:46 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SetUp from './tutorial/5-useRef/setup/1-useRef-basics'
+import SetUp from './tutorial/8-useContext/setup/1-context-api'
 
 function App() {
   return(
