3bfba91f893135725b5d8eec7ee7ef8851cc5c1c
sushmitha <sushmithakori99@gmail.com>
2020-12-25 19:09:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SetUp from './tutorial/11-react-router/setup/index'
+import SetUp from './tutorial/12-memo-useMemo-useCallback/setup/index'
 
 function App() {
   return(
--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 
-const Modal = (modalContent,closeModal) => {
-    useEffect(()=>{
-      setTimeout(()=>{
-          closeModal();
-      },3000);
-    })
-    return(
-        <div className="item">
-            <p>{modalContent}</p>
-        </div>
-    );
+const Modal = ({ modalContent, closeModal }) => {
+     useEffect(() => {
+     setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+  return (
+     <div className='modal'>
+       <p>{modalContent}</p>
+    </div>
+  );
 };
 
 export default Modal;
